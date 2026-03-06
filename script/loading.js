8372382cf34fc49373f4ea178216eab63cad449a
HendrikCoetzee-ui <hbc.coetzee@gmail.com>
2026-03-06 10:04:21 -0800

    Save

--- script/loading.js
+++ script/loading.js
@@ -7,9 +7,9 @@ int = setInterval(blurring, 30)
 
 function blurring() {
     load = load + 1
-    loadText.innerText = ${load}%
+    loadText.innerText = `${load}%`
     loadText.style.opacity = 1-load/100
-    bg.style.filter = blur(${50-load/2}px)
+    bg.style.filter = `blur(${50-load/2}px)`
 
     if (load > 99) {
         clearInterval(int)
