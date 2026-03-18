e4501fc4f7fa2088f782d1aa44998193e6ce1666
HendrikCoetzee-ui <hbc.coetzee@gmail.com>
2026-03-18 16:58:27 +0000

    Wed, Mar 18, 2026, 9:58 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -1,3 +1,8 @@
 screens = document.querySelectorAll('.screen')
 choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
 start_btn = document.getElementById('start-btn')
+
+
+start_btn.addEventListener('click', () => {
+    screens[0].classList.add('up')
+})
