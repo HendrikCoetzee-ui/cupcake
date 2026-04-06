a9e440939e4488fd0481fa0a4493de680ff55c7b
HendrikCoetzee-ui <hbc.coetzee@gmail.com>
2026-04-06 22:33:03 +0000

    Mon, Apr 6, 2026, 3:33 PM -07:00

--- script/nav.js
+++ script/nav.js
@@ -1,11 +1,10 @@
 nav = document.querySelector('.nav')
 
 window.addEventListener('scroll', () => {
-    console.log(scrollY)
-    if (scrollY > 500) {
+    if(scrollY > 520) {
         nav.classList.add('active')
     }
     else {
         nav.classList.remove('active')
     }
-    })
+})
